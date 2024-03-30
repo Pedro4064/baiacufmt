f9a151bea2c2379320c82440dde00f04872ea5ee
Pedro4064 <pedrohlcruz@gmail.com>
2024-03-30 10:30:24 -0300

    [IMPLEMENTED] Formatting function

--- src/extension.ts
+++ src/extension.ts
@@ -33,7 +33,7 @@ const function_name_extract = (function_definition: string) => {
 		console.log(function_name); // Output: "void"
 		return function_name;
 	}
-}
+};
 
 const function_arguments_extract = (function_definition: string) => {
 	const arguments_regex = /\((.*)\)/;
@@ -48,7 +48,7 @@ const function_arguments_extract = (function_definition: string) => {
 		console.log(function_arguments); // Output: "void"
 		return function_arguments;
 	}
-}
+};
 
 const get_variables_to_be_used = async (variables_str: string) => {
 	const variables = variables_str.split(',');
@@ -71,21 +71,21 @@ const get_variables_to_be_used = async (variables_str: string) => {
 	console.log(variables_information);
 	return variables_information;
 
-}
+};
 
-const full_justify_text = (text: string, offset: number) => {
+const full_justify_text = (text: string, offset: number, text_ending: string, text_start: string, max_length_deduct: number) => {
 	const words = text.split(' ');
 	let formatted_text = '';
 	let current_line = '';
 	let line_length = 0;
-	let max_length = 36;
+	let max_length = 37 - max_length_deduct;
 
 	for (const word of words) {
 		if ((current_line + word).length <= max_length) {
 			current_line += word + ' ';
 			line_length += word.length + 1; // Add word length and space
 		} else {
-			const number_of_spaces = 40 - line_length + current_line.trim().split(' ').length - 1;
+			const number_of_spaces = max_length - line_length + current_line.trim().split(' ').length - 1;
 			const words_in_line = current_line.trim().split(' ').length;
 			const spaces_between_words = Math.floor(number_of_spaces / (words_in_line - 1));
 			const extra_spaces = number_of_spaces % (words_in_line - 1);
@@ -98,7 +98,7 @@ const full_justify_text = (text: string, offset: number) => {
 			}
 			line += words_array[words_array.length - 1]; // Add last word without trailing space
 
-			formatted_text += ' '.repeat(offset) + line + '\n';
+			formatted_text += text_start + ' '.repeat(offset) + line + text_ending + '\n';
 			current_line = word + ' ';
 			line_length = word.length + 1;
 		}
@@ -106,13 +106,29 @@ const full_justify_text = (text: string, offset: number) => {
 
 	// Add the last line
 	const last_line = current_line.trim();
-	formatted_text += ' '.repeat(offset) + last_line + ' '.repeat(max_length - last_line.length);
+	var ending_white_spaces = max_length - max_length_deduct - last_line.length;
+	ending_white_spaces = (ending_white_spaces >= 0) ? ending_white_spaces : 0;
+
+	formatted_text += text_start + ' '.repeat(offset) + last_line + ' '.repeat(ending_white_spaces) + text_ending + '\n';
 
 	return formatted_text;
-}
+};
+
+const format_variables = (variables: [string, string][]) => {
+	var formatted_text: string = '';
+	for (const [index, [variable_name, variable_description]] of variables.entries()) {
+
+		formatted_text += (index === 0) ? full_justify_text(variable_name, 0, '  //', '', 0) : full_justify_text(variable_name, 23, '  //', '//', 1);
+		formatted_text += full_justify_text(variable_description, 24, ' //', '//', 1);
+
+	}
+
+	return formatted_text;
+};
+
 // This method is called when your extension is activated
 // Your extension is activated the very first time the command is executed
-export function activate(context: vscode.ExtensionContext) {
+export async function activate(context: vscode.ExtensionContext) {
 
 	// Use the console to output diagnostic information (console.log) and errors (console.error)
 	// This line of code will only be executed once when your extension is activated
@@ -121,7 +137,7 @@ export function activate(context: vscode.ExtensionContext) {
 	// The command has been defined in the package.json file
 	// Now provide the implementation of the command with registerCommand
 	// The commandId parameter must match the command field in package.json
-	let disposable = vscode.commands.registerCommand('baiacufmt.helloWorld', () => {
+	let disposable = vscode.commands.registerCommand('baiacufmt.helloWorld', async () => {
 		// The code you place here will be executed every time your command is executed
 		// Display a message box to the user
 		const editor = vscode.window.activeTextEditor;
@@ -136,14 +152,20 @@ export function activate(context: vscode.ExtensionContext) {
 			const function_name = function_name_extract(line);
 
 			const raw_function_arguments = function_arguments_extract(line);
-			const variables_and_descriptions = get_variables_to_be_used(raw_function_arguments);
+			const variables_and_descriptions: [string, string][] = await get_variables_to_be_used(raw_function_arguments);
 
 			// Format Final Comment Block
-			const template_footer_and_header = '// ***************************************************** //';
+			const template_footer_and_header = '// ********************************************************** //';
 			const template_method_name = '// Method name:        ';
 			const template_method_description = '// Method description: '
 			const template_input_params = '// Input params:       '
 			const template_output_params = '// Output params:       '
+			var comment_block: string = '';
+
+			comment_block += template_footer_and_header + '\n';
+			comment_block += template_method_name + function_name + '//\n'
+			comment_block += template_input_params + format_variables(variables_and_descriptions);
+			console.log(comment_block);
 		}
 
 
